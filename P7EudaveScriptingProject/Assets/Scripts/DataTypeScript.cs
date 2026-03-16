3b053152d1cc5f45c8ac632b158b026bf8c60700
Kaylin E <768323@student.dusd.net>
2026-03-16 14:23:48 -0700

    P7
    Added a script, still working

---
+++ P7EudaveScriptingProject/Assets/Scripts/DataTypeScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class DataTypeScript : MonoBehaviour
+{
+    void Start()
+    {
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
+    }
+}
